86c8c2232714064c8dce71c010d6ab2645a4f74d
Thom Wiggers <thom@thomwiggers.nl>
2014-01-01 17:46:25 +0100

    Reboot into IRC bot

---
+++ README.md
@@ -0,0 +1,5 @@
+Aeolus
+======
+
+This is my second attemt at building an IRC bot. This time I will do it in
+python. I will be doing TDD.
---
+++ tests/test_simple.py
@@ -0,0 +1,10 @@
+import unittest
+
+class Tests(unittest.TestCase):
+
+  def test_one_plus_one(self):
+    """
+    Basically only assert that 
+    tests are working
+    """
+    self.assertEqual(2, 1+1)
